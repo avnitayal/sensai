3b571d9d81cbf95366e4f987199cd81469528e61
avnitayal <146936540+avnitayal@users.noreply.github.com>
2025-09-21 10:36:46 +0530

    Update route.js

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,8 +1,7 @@
 import { serve } from "inngest/next";
-
 import { inngest } from "@/lib/inngest/client";
-import { generateIndustryInsights } from "../../lib/inngest/function.js";
-
+// Fixed relative path: now goes up three levels to reach the root lib folder
+import { generateIndustryInsights } from "../../../lib/inngest/function";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
